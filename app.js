2a56208599507caaab12140906f0f1456cbce597
rgstech <guimaorod@gmail.com>
2022-04-30 15:20:45 -0300

    created user model, controller, routes and middleware files and auth from db

--- app.js
+++ app.js
@@ -5,55 +5,69 @@
 */
 
 
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const express             = require("express");
 
-const indexRouter = require("./routes/index");
-const clientesRouter = require("./routes/clientes");
-const jwt = require("jsonwebtoken");
+const path                = require("path");
 
-const verifyJWT = require('./middlewares/authjwt');
+const cookieParser        = require("cookie-parser");
 
-const  blacklist = require('./util/blacklist');
+const logger              = require("morgan");
 
-const SECRET = require('./config/globals').SECRET;
+const indexRouter         = require("./routes/index");
+
+const clientesRouter      = require("./routes/clientes");
+
+const jwt                 = require("jsonwebtoken");
+
+const verifyJWT           = require('./middlewares/authjwt');
+
+const blacklist           = require('./util/blacklist');
+
+const SECRET              = require('./config/globals').SECRET;
+
+const userModel           = require('./models/UserModel');
+
+const userController      = require('./controllers/UserController')(userModel);
+
+
 
 const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-// test middleware
-// const teste = function (req, res, next) {
-//     console.log(' Passei aqui as :', Date.now());
-//     next();
-//   }
-
 
 
 app.use("/login", (req, res) => {
 
-  if (req.body.user == "rodrigo" && req.body.password == "123") { // for test/example only, use a database to authenticate user!
-                                                                  // somente para teste/exemplo, use o banco de dados para autenticar o usuario! 
-    const token = jwt.sign({ userId: 1 }, SECRET, { expiresIn: 600 });
 
-    res.json({ auth: true, token });
-  }
+  userController.getByUser(req.body.name, (vuser) => {
+        
+              if (vuser.dataValues['password'] == req.body.password) {
 
-  res.status(401).end();
+                const token = jwt.sign({ userId: 1 }, SECRET, { expiresIn: 600 });
+     
+                res.json({ auth: true, token });
+
+              } else {
+
+                res.status(401).end();
+              
+              }
+              
+  });
   
 });
 
 
 
 app.use("/logout", (req, res) => { 
+
    blacklist.push(req.headers['x-access-token']);
    res.json({msg: 'you logout!'});
+
    
 });
 
